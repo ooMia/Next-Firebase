be61383db539e624f758f5350466fedd4d54bac8
Hyeon-hak Kim <07ily@naver.com>
2023-09-15 14:32:29 +0900

    Update firebase.ts

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -3,9 +3,7 @@ import {initializeApp} from 'firebase/app';
 import { getFirestore } from "firebase/firestore";
 import {FirebaseOptions} from '@firebase/app-types';
 
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig: FirebaseOptions = {
